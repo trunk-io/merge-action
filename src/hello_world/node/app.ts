723eaf95c9cc44828b0275dfe8ee09b49e212562
Nikhil Birmiwal <nikhil@trunk.io>
2023-07-13 18:26:25 -0700

    save

--- src/hello_world/node/app.ts
+++ src/hello_world/node/app.ts
@@ -1,8 +1,11 @@
 import { HELLO_WORLD } from "./lib";
 import chai from "chai";
+import { describe } from "mocha";
 
 try {
   chai.expect(HELLO_WORLD).to.equal("Goodbye, world!");
 } catch (err) {
   console.log("Caught an err!");
 }
+
+describe("TestSuite", async function () {});
