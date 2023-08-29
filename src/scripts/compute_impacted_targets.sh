ea4b1f38c2081f7e5472cd34be72be67373736e1
Nikhil Birmiwal <nikhil@trunk.io>
2023-08-29 15:35:00 -0700

    fix bazel version reporting

--- src/scripts/compute_impacted_targets.sh
+++ src/scripts/compute_impacted_targets.sh
@@ -38,7 +38,7 @@ fetchRemoteGitHistory "${PR_BRANCH}"
 # Install the bazel-diff JAR. Avoid cloning the repo, as there will be conflicting WORKSPACES.
 curl --retry 5 -Lo bazel-diff.jar https://github.com/Tinder/bazel-diff/releases/latest/download/bazel-diff_deploy.jar
 java -jar bazel-diff.jar -V
-bazel -- -V
+bazel --version
 
 git switch "${MERGE_INSTANCE_BRANCH}"
 merge_instance_branch_head_sha=$(git rev-parse "${MERGE_INSTANCE_BRANCH}")
