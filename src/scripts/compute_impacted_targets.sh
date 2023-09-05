fa47348d857ec939a1b11415fdef4452e5932763
Nikhil Birmiwal <nikhil@trunk.io>
2023-09-05 15:00:30 -0700

    remove remaining java invocations

--- src/scripts/compute_impacted_targets.sh
+++ src/scripts/compute_impacted_targets.sh
@@ -85,11 +85,6 @@ if [[ -n ${VERBOSE} ]]; then
 	git log -n "${pr_depth}" --oneline
 fi
 
-# Install the bazel-diff JAR. Avoid cloning the repo, as there will be conflicting WORKSPACES.
-curl --retry 5 -Lo bazel-diff.jar https://github.com/Tinder/bazel-diff/releases/latest/download/bazel-diff_deploy.jar
-_java -jar bazel-diff.jar -V
-bazel version # Does not require running with startup options.
-
 # Output Files
 merge_instance_branch_out=./${merge_instance_branch_head_sha}
 merge_instance_with_pr_branch_out=./${pr_branch_head_sha}_${merge_instance_branch_head_sha}
