bf4d36783f995aa6bcd7e4fe97af58f978d7fa01
Nikhil Birmiwal <nikhil@trunk.io>
2023-09-12 15:55:41 -0700

    save

--- src/scripts/compute_impacted_targets.sh
+++ src/scripts/compute_impacted_targets.sh
@@ -53,7 +53,7 @@ ifVerbose git status
 
 # NOTE: We cannot assume that the checked out Git repo (e.g. via actions-checkout)
 # was a shallow vs a complete clone. The `--depth` options deepens the commit history
-ss# in both clone modes: https://git-scm.com/docs/fetch-options#Documentation/fetch-options.txt---depthltdepthgt
+# in both clone modes: https://git-scm.com/docs/fetch-options#Documentation/fetch-options.txt---depthltdepthgt
 fetchRemoteGitHistory() {
 	logIfVerbose "Fetching" "$@" "..."
 	git pull
