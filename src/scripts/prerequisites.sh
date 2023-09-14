2114b7fcdaf19bbd8c0e106f5b1d753ee57a10f7
Nikhil Birmiwal <nikhil@trunk.io>
2023-09-14 15:29:24 -0700

    verbose logging

--- src/scripts/prerequisites.sh
+++ src/scripts/prerequisites.sh
@@ -2,13 +2,16 @@
 
 set -euo pipefail
 
+pwd
+ls -alr
+
 # NOTE: We cannot assume that the checked out Git repo (e.g. via actions-checkout)
 # was a shallow vs a complete clone. The `--depth` options deepens the commit history
 # in both clone modes: https://git-scm.com/docs/fetch-options#Documentation/fetch-options.txt---depthltdepthgt
 fetchRemoteGitHistory() {
-	logIfVerbose "Fetching" "$@" "..."
-	git fetch --quiet --depth=2147483647 origin "$@"
-	logIfVerbose "...done!"
+	echos "Fetching" "$@" "..."
+	git fetch --depth=2147483647 origin "$@"
+	echo "...done!"
 }
 
 pr_branch="${PR_BRANCH}"
