4ec87a97079ee3fbf55cc3bd7d105bb8bc70671d
Samuel Lijin <github@sxlijin.com>
2024-01-12 10:14:10 -0800

    debug

--- src/scripts/prerequisites.sh
+++ src/scripts/prerequisites.sh
@@ -13,7 +13,7 @@ fetchRemoteGitHistory() {
 any_error_occurred=false
 
 if [[ -z ${API_TOKEN} ]]; then
-	echo "You must specify a `trunk-token` for trunk-io/merge-action to use"
+	echo "You must specify a 'trunk-token' for trunk-io/merge-action to use"
 	any_error_occurred=true
 fi
 
