4d3639d0ea62e07dd50e073228d13e8b95d2ae72
Phil Vendola <pv72895@gmail.com>
2024-02-14 15:32:15 +0000

    Use checkout instead of switch

--- src/scripts/compute_impacted_targets.sh
+++ src/scripts/compute_impacted_targets.sh
@@ -53,15 +53,6 @@ bazelDiff() {
 	fi
 }
 
-# NOTE: We cannot assume that the checked out Git repo (e.g. via actions-checkout)
-# was a shallow vs a complete clone. The `--depth` options deepens the commit history
-# in both clone modes: https://git-scm.com/docs/fetch-options#Documentation/fetch-options.txt---depthltdepthgt
-fetchRemoteGitHistory() {
-	logIfVerbose "Fetching" "$@" "..."
-	git fetch --quiet --depth=2147483647 origin "$@"
-	logIfVerbose "...done!"
-}
-
 ## Verbose logging for the Merge Instance and PR branch.
 if [[ -n ${VERBOSE} ]]; then
 	# Find the merge base of the two branches
@@ -72,14 +63,14 @@ if [[ -n ${VERBOSE} ]]; then
 	merge_instance_depth=$(git rev-list "${merge_base_sha}".."${MERGE_INSTANCE_BRANCH_HEAD_SHA}" | wc -l)
 	echo "Merge Instance Depth= ${merge_instance_depth}"
 
-	git switch "${MERGE_INSTANCE_BRANCH}"
+	git checkout "${MERGE_INSTANCE_BRANCH}"
 	git log -n "${merge_instance_depth}" --oneline
 
 	# Find the number of commits between the merge base and the PR's HEAD
 	pr_depth=$(git rev-list "${merge_base_sha}".."${PR_BRANCH_HEAD_SHA}" | wc -l)
 	echo "PR Depth= ${pr_depth}"
 
-	git switch "${PR_BRANCH}"
+	git checkout "${PR_BRANCH}"
 	git log -n "${pr_depth}" --oneline
 fi
 
